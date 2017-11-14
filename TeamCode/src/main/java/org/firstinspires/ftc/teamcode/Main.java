907471dd0d60154171959ffb64070dc56b5d77a7
perbhat <perbhatk@gmail.com>
2017-11-14 16:46:52 -0500

    Adding Encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -1,78 +1,87 @@
+/*
+Copyright 2017 FIRST Tech Challenge Team 12365
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.ServoController;
+import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.*;
 
-
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Remove a @Disabled the on the next line or two (if present) to add this opmode to the Driver Station OpMode list,
+ * or add a @Disabled annotation to prevent this OpMode from being added to the Driver Station
+ */
 @TeleOp
 
 public class Main extends LinearOpMode {
+    private DcMotor tLeftDrive;
+    private DcMotor tRightDrive;
+    private DcMotor bLeftDrive;
+    private DcMotor bRightDrive;
+    private DcMotor arm;
+    private Servo cubeGrabber;
+    private Servo grabber;
 
-    // todo: write your code here
-    
-
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor tLeftDrive = null;
-    private DcMotor tRightDrive = null;
-    private DcMotor bLeftDrive = null;
-    private DcMotor bRightDrive = null;
-    private Servo grabber = null;
-    private Servo cubeGrabber = null;
-    private DcMotor arm = null;
 
     @Override
     public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        tLeftDrive  = hardwareMap.get(DcMotor.class, "tLeftDrive");
+        tLeftDrive = hardwareMap.get(DcMotor.class, "tLeftDrive");
         tRightDrive = hardwareMap.get(DcMotor.class, "tRightDrive");
         bLeftDrive = hardwareMap.get(DcMotor.class, "bLeftDrive");
         bRightDrive = hardwareMap.get(DcMotor.class, "bRightDrive");
-        grabber = hardwareMap.get(Servo.class, "grabber");
-        cubeGrabber = hardwareMap.get(Servo.class, "cubeGrabber");
         arm = hardwareMap.get(DcMotor.class, "arm");
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        tLeftDrive.setDirection(DcMotor.Direction.FORWARD);
-        tRightDrive.setDirection(DcMotor.Direction.REVERSE);
-        bLeftDrive.setDirection(DcMotor.Direction.FORWARD);
-        bRightDrive.setDirection(DcMotor.Direction.REVERSE);
-        arm.setDirection(DcMotor.Direction.FORWARD);
+        cubeGrabber = hardwareMap.get(Servo.class, "cubeGrabber");
+        grabber = hardwareMap.get(Servo.class, "grabber");
         
         
-        
-        
-        
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        runtime.reset();
         
-        double tgtPower = 0; // servo stuff
-        int servoPower = 0; // servo stuff pt 2
-        
-        
+        // arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+         
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            
-           
+            telemetry.addData("Status", "Running");
+            telemetry.update();
             
             // Setup a variable for each drive wheel to save power level for telemetry
+            int position = arm.getCurrentPosition();
             double tLeftPower;
             double tRightPower;
             double bLeftPower;
             double bRightPower;
-            double arm;
+           // double arm;
 
             // Choose to drive using either Tank Mode, or POV Mode
             // Comment out the method that's not used.  The default below is POV.
@@ -84,7 +93,7 @@ public class Main extends LinearOpMode {
             boolean turnRight  =  gamepad1.dpad_right;
             boolean turnLeft  =  gamepad1.dpad_left;
             boolean stopRight  =  !gamepad1.dpad_right;
-            double armPower = gamepad1.right_trigger;
+           // double armPower = gamepad1.right_trigger;
             
                 
              
@@ -95,19 +104,16 @@ public class Main extends LinearOpMode {
             tRightPower   = Range.clip(drive - side, -1.0, 1.0) ;
             bLeftPower    = Range.clip(drive + side, -1.0, 1.0) ;
             bRightPower   = Range.clip(drive - side, -1.0, 1.0) ;
-            arm           = Range.clip(armPower, -1.0, 1.0);
+          //  arm           = Range.clip(armPower, -1.0, 1.0);
             
         
             while (turnLeft){
             tLeftDrive.setPower(1.0);
             tRightDrive.setPower(-1.0);
             bLeftDrive.setPower(1.0);
-            bRightDrive.setPower(1.0);
+            bRightDrive.setPower(-1.0);
                 if(gamepad1.dpad_up){
-            tLeftDrive.setPower(0);
-            tRightDrive.setPower(0);
-            bLeftDrive.setPower(0);
-            bRightDrive.setPower(0);
+        
                     turnLeft = false;
                 }
             
@@ -152,19 +158,39 @@ public class Main extends LinearOpMode {
             else if (gamepad1.x){
                 grabber.setPosition(1);
             }
-
+        
+            
+         if(gamepad1.a){
+           
+            arm.setPower(1.0);
+            arm.setTargetPosition(400);
+            
+            gamepad1.a = false;
+            
+        }
+        else if (gamepad1.b){
+            
+            arm.setPower(1.0);
+            arm.setTargetPosition(10);
+            
+            gamepad1.b = false;
+            
+        }
             
 
 // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "topLeft (%.2f), topRight (%.2f), bottomLeft (%.2f), bottomRight (%.2f)", tLeftPower, tRightPower, bLeftPower, bRightPower);
+        //    telemetry.addData("Status", "Run Time: " + runtime.toString());
+            // telemetry.addData("Motors", "tLeft (%.2f), topRight (%.2f), bottomLeft (%.2f), bottomRight (%.2f)", tLeftPower, tRightPower, bLeftPower, bRightPower);
             telemetry.addData("Servo Position", grabber.getPosition());
+            telemetry.addData("Encoder Position", position);
             telemetry.addData("Cube Grabber Position", cubeGrabber.getPosition());
-            telemetry.addData("Target Power", tgtPower);
+          //  telemetry.addData("Target Power", tgtPower);
             telemetry.addData("Status", "Running");
+            telemetry.addData("Back Left Wheel:", bLeftDrive);
             telemetry.update();
     
 }
 
 }
-}
+
+        }
